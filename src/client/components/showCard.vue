9b921f13a43c248bf11b9623bd4cc2c6e00f25ca
Aos <aosdab@gmail.com>
2017-07-08 14:12:37 -0400

    Fixed card retrieval warning

--- src/client/components/showCard.vue
+++ src/client/components/showCard.vue
@@ -1,24 +1,29 @@
 <template>
   <div>
-    <br>
-    <div v-if="flipped" class="container">
-      <div class="box">
-        <pre v-if="card.code"><code class="preserve-ws">{{card.back}}</code></pre>
-        <p class="preserve-ws center-vertical" v-else>{{card.back}}</p>
-      </div>
+    <div v-if="!card">
+      Loading card...
     </div>
-    <div v-else class="container">
-      <div class="box">
-        <h1 class="title has-text-centered center-vertical is-2">{{card.front}}</h1>
+    <div v-else>
+      <br>
+      <div v-if="flipped" class="container">
+        <div class="box">
+          <pre v-if="card.back"><code class="preserve-ws">{{card.back}}</code></pre>
+          <p class="preserve-ws center-vertical" v-else>{{card.back}}</p>
+        </div>
       </div>
-    </div>
-    <div class="container has-text-centered bottom-buttons">
-      <a class="button is-medium is-info" @click.prevent="flipped = !flipped">⇆ Flip Card</a>
-      <a @click.prevent="card.known = !card.known" class="button is-medium" :class="card.known ? 'is-success' : 'is-light'">
-        <span v-if="card.known">✓ Known</span>
-        <span v-else>I Know It!</span>
-      </a>
-      <a href="#" class="button is-medium is-warning">Next Card →</a>
+      <div v-else class="container">
+        <div class="box">
+          <h1 class="title has-text-centered center-vertical is-2">{{card.front}}</h1>
+        </div>
+      </div>
+      <div class="container has-text-centered bottom-buttons">
+        <a class="button is-medium is-info" @click.prevent="flipped = !flipped">⇆ Flip Card</a>
+        <a @click.prevent="card.known = !card.known" class="button is-medium" :class="card.known ? 'is-success' : 'is-light'">
+          <span v-if="card.known">✓ Known</span>
+          <span v-else>I Know It!</span>
+        </a>
+        <a href="#" class="button is-medium is-warning">Next Card →</a>
+      </div>
     </div>
   </div>
 </template>
@@ -31,7 +36,7 @@ export default {
       flipped: false,
       general_card: false,
       code_card: true,
-      random_card: false
+      random_card: false,
     }
   },
   computed: {
@@ -40,9 +45,6 @@ export default {
     }
   },
   methods: {
-  },
-  created() {
-    this.card = this.$store.getters.cardById(this.$route.params.id);
   }
 }
 </script>
--- src/client/store/getters.js
+++
@@ -1,3 +0,0 @@
-export const cardById = (state, getters) => (id) => {
-  return state.allCards.find((card) => card._id === id)
-}
